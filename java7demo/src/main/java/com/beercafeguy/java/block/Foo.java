eacea54d0e3e71ca455f422b2a1b2e85c2db30f5
beercafeguy <hemchandra@outlook.com>
2018-08-26 21:26:31 +0530

    static , ini block and constructor

---
+++ java7demo/src/main/java/com/beercafeguy/java/block/Foo.java
@@ -0,0 +1,29 @@
+package com.beercafeguy.java.block;
+
+public class Foo {
+    {
+        System.out.println("Ini block 1");
+    }
+    {
+        System.out.println("Ini block 0");
+    }
+
+
+    static {
+        System.out.println("Static block 1");
+    }
+    static {
+        System.out.println("Static block 0");
+    }
+
+
+
+    public Foo(){
+        System.out.println("This is foo");
+    }
+
+    public static void main(String[] args) {
+        Foo foo1=new Foo();
+        Foo foo2=new Foo();
+    }
+}
