55b4b91009a625838537fac3733bead9792cef44
beercafeguy <hemchandra@outlook.com>
2018-08-29 22:36:29 +0530

    sum of two bytes should be int as you may loose data because of precision. To be exact sum of any data types smaller then int should be int

---
+++ java7demo/src/main/java/com/beercafeguy/java/assign/ByteDemo.java
@@ -0,0 +1,11 @@
+package com.beercafeguy.java.assign;
+
+public class ByteDemo {
+    public static void main(String[] args) {
+        byte b=10;
+        byte c=2;
+        //byte d=b+c;
+        //above line is illegal because sub of two bytes should be int
+        System.out.println(b);
+    }
+}
