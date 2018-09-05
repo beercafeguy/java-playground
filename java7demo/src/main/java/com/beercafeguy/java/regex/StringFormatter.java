1fcd85524cf2b8c237323fc9bc31165c5f392d48
beercafeguy <hemchandra@outlook.com>
2018-09-05 22:45:19 +0530

    format string using Formatter class or prontf method in PrintStream class

---
+++ java7demo/src/main/java/com/beercafeguy/java/regex/StringFormatter.java
@@ -0,0 +1,7 @@
+package com.beercafeguy.java.regex;
+
+public class StringFormatter {
+    public static void main(String[] args) {
+        System.out.printf("%2$d + %1$d = %3$d",105,220,(105+220));
+    }
+}
