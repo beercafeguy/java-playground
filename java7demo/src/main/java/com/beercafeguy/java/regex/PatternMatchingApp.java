ff023d293f179758f575ecb77adf8f4c4c6d6c34
beercafeguy <hemchandra@outlook.com>
2018-09-05 22:09:05 +0530

    HAXAdecimal match

--- java7demo/src/main/java/com/beercafeguy/java/regex/PatternMatchingApp.java
+++ java7demo/src/main/java/com/beercafeguy/java/regex/PatternMatchingApp.java
@@ -4,6 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 // \d+ d12c3m will return 1 4
+//Source: abc 0XDC8
+//Pattern: 0[xX][0-9a-fA-F]
+ // 4 0XD
 public class PatternMatchingApp {
     public static void main(String[] args) {
         Pattern pattern=Pattern.compile(args[0]);
@@ -12,7 +15,7 @@ public class PatternMatchingApp {
         System.out.println("Source: "+args[1]);
         System.out.println("Pattern: "+matcher.pattern());
         while(matcher.find()){
-            System.out.print(matcher.start()+" ");
+            System.out.println(matcher.start()+" "+matcher.group());
         }
     }
 }
