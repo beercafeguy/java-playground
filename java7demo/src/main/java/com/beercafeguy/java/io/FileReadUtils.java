4ba4bdb62ab298946b61bc1b1911157cdd84c9f8
beercafeguy <hemchandra@outlook.com>
2018-09-08 16:12:50 +0530

    added logic to read the files using Buffered Reader

--- java7demo/src/main/java/com/beercafeguy/java/io/FileReadUtils.java
+++ java7demo/src/main/java/com/beercafeguy/java/io/FileReadUtils.java
@@ -1,7 +1,29 @@
 package com.beercafeguy.java.io;
 
+import java.io.*;
+
 public class FileReadUtils {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        File file1=new File("file1.txt");
+
+
+        //Read using File Reader
+        char[] in=new char[100];
+        FileReader fr=new FileReader(file1);
+        int size=fr.read(in);
+        System.out.println("Number of chars: "+size);
+        for(char c:in)
+            //System.out.println(c);
+        fr.close();
+
+        //Read using buffered Reader
+        BufferedReader br=new BufferedReader
+                (new FileReader(new File("file2.txt")));
+        String s;
+        while((s=br.readLine())!=null){
+            System.out.println(s);
+        }
+        br.close();
+
     }
 }
